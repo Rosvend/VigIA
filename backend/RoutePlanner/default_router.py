b5bc2dfe41c4c320522ead129f23ce1a5eff08df
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-02-27 18:31:21 -0500

    Add basic file hierarchy for backend component

---
+++ backend/RoutePlanner/default_router.py
@@ -0,0 +1,4 @@
+class PoliceRouter:
+    def compute_routes(self):
+        # TODO: implement route computation
+        return [[(-1.0, 4.0), (3.0, 0.5), (2.5, 9.0)]]
--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, request
+from RoutePlanner.default_router import PoliceRouter
+from markupsafe import escape
+app = Flask(__name__)
+__version__ = "0.0.1"
 
+route_computer = PoliceRouter()
+
+@app.route("/")
+def index():
+    return f"Running crime backend version {__version__}</p>"
+
+@app.route("/routes/<int:area>")
+def getRoutes(area):
+    route_qty = request.args.get("n", "")
+    return route_computer.compute_routes()
