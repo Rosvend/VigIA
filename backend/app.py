e991631f76438cf3b393db97729eb0ff2bc3dcd0
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-03-07 04:13:04 -0500

    Separate documentation from app.py

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SWAGGER'] = {
     'title': 'Patrol Routes API',
-    'uiversion': 3
+    'uiversion': 3,
 }
 swagger = Swagger(app)
 
@@ -29,99 +29,28 @@ class RouteSuggestions(Resource):
                                  type=int,
                                  location='args')
 
-    @swag_from({
-        'summary': 'Retornar una lista de hostspots y rutas '
-            + 'sugeridas para un determinado cai.',
-        'description': 'Este endpoint recibe el id de un'
-            + 'determinado cai y una cantidad de rutas a '
-            + 'computar. Devuelve esta cantidad de rutas optimizadas '
-            + 'para cubrir los hotspots indicados, todas partiendo '
-            + 'del cai designado. ',
-        'parameters': [
-            {
-                'in': 'query',
-                'name': 'cai',
-                'schema': { 'type': 'integer' },
-                'description': 'Id del cai del cual partir.',
-                'required': True
-            },
-            {
-                'in': 'query',
-                'name': 'n',
-                'schema': {'type': 'integer'},
-                'description': 'Número de rutas a generar.',
-                'required': False
-            }
-        ],
-        'responses': {
-            '200': {
-                'description': 'OK',
-                'content': {
-                    'application/json': {
-                        'schema': {
-                            'type': 'object',
-                            'properties': {
-                                'cai': {
-                                    'type': 'array',
-                                },
-                                'hotspots': {
-                                    'type': 'array',
-                                },
-                                'routes': {
-                                    'type': 'array',
-                                }
-                            }
-                        },
-                        'examples': {
-                            'example1': {
-                                'summary': 'Successful response',
-                                'value': {
-                                    'cai': [23.4, -28.09],
-                                    'hotspots': [
-                                        {
-                                            'coordinates': [56.8, -38.3],
-                                            'probability': 0.34
-                                        },
-                                        {
-                                            'coordinates': [36.9, -18.5],
-                                            'probability': 0.9
-                                        },
-                                    ],
-                                    'routes': [
-                                        [
-                                            [56.3, 83.4],
-                                            [90.3, 32.2],
-                                            [482.3, 484.3]
-                                        ]
-                                    ]
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    })
+    
+    @swag_from("doc/RouteSuggestions_get.yml")
     def get(self):
         args = self.parser.parse_args()
         # Todo: use real values here
         return {
-            'cai': [23.4, -28.09],
+            'cai': { 'lat': 23.4, 'lon': -28.09},
             'hotspots': [
                 {
-                    'coordinates': [56.8, -38.3],
+                    'coordinates': { 'lat': 56.8, 'lon': -38.3},
                     'probability': 0.34
                 },
                 {
-                    'coordinates': [36.9, -18.5],
+                    'coordinates': { 'lat': 36.9, 'lon': -18.5},
                     'probability': 0.9
                 },
             ],
             'routes': [
                 [
-                    [56.3, 83.4],
-                    [90.3, 32.2],
-                    [482.3, 484.3]
+                    { 'lat': 56.3, 'lon': 83.4},
+                    { 'lat': 90.3, 'lon': 32.2},
+                    { 'lat': 482.3, 'lon': 484.3}
                 ]
             ] * (args['n'] if args['n'] is not None else 1)
         };
