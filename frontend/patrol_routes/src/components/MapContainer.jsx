5d94afdb070d537b122a1471ca06298927ea1c65
Argenis <argenis.omana@upb.edu.co>
2025-03-27 18:39:39 -0500

    commit4

--- frontend/patrol_routes/src/components/MapContainer.jsx
+++ frontend/patrol_routes/src/components/MapContainer.jsx
@@ -2,7 +2,7 @@ function MapContainer({ marginLeft }) {
   return (
     <div id="map-container" style={{ marginLeft }}>
       <div id="map">
-        {/* Replace with actual map implementation */}
+        {/*map implementation*/}
         <img
           src="/assets/MAP_DEMOSTRATION.png"
           alt="Mapa de rutas de patrulla"
