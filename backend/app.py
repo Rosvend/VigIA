85628bb5b7702310a39a731afe0bcdb1a9bc5c91
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-04-06 16:33:17 -0500

    Add JWT-based initial authentication mechanism

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_cors import CORS
+import flask_login
 from RoutePlanner.default_router import PoliceRouter, ORS_PROFILES, DEFAULT_ORS_PROFILE
 from markupsafe import escape
 from flask_restful import Resource, Api
 from flask_restful.reqparse import RequestParser
 from flasgger import Swagger, swag_from
+import os
+from database.models import Manager
+from werkzeug.security import check_password_hash as check_pw
+import datetime
+import jwt
+
+TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = "HS256"
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportException:
+    pass
 
 __version__ = "0.0.2"
 
@@ -17,6 +32,21 @@ app.config['SWAGGER'] = {
 }
 swagger = Swagger(app)
 
+app.secret_key = os.getenv('APP_SECRET_KEY')
+if len(app.secret_key) == 0:
+    raise EnvironmentError("No APP_SECRET_KEY provided.")
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+@login_manager.request_loader
+def request_loader(request):
+    try:
+        payload = jwt.decode(request.authorization.token,
+                             app.secret_key,
+                             algorithms=[ALGORITHM])
+        return Manager.get(Manager.cedula == payload.get("sub"))
+    except:
+        return None
 
 class RouteSuggestions(Resource):
     route_computer = PoliceRouter()
@@ -55,6 +85,48 @@ class RouteSuggestions(Resource):
                 else 0.0
         )
 
+class Route(Resource):
+    def get(self, date, cai_id, assigned_to):
+        try:
+            date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        except ValueError:
+            abort(400)
+        return {
+            'date': date.strftime('%Y/%m/%d'),
+            'cai_id': cai_id,
+            'assigned_to': assigned_to,
+        }
+
+    @flask_login.login_required
+    def put(self, date, cai_id, assigned_to):
+        return flask_login.current_user.cedula
+
+
+@app.post('/api/admin/login')
+def admin_login():
+    # Use 'username' here for Oauth2 compliance
+    cedula = request.form['username']
+    password = request.form['password']
+
+    try:
+        manager = Manager.get(Manager.cedula == cedula)
+    except:
+        return {"error": "Wrong credentials"}, 401
+    if check_pw(manager.password_hash, password):
+        expires_at = datetime.datetime.now(datetime.timezone.utc) \
+            + datetime.timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+        token = jwt.encode({"sub": manager.cedula, "exp": expires_at},
+                           app.secret_key,
+                           algorithm=ALGORITHM)
+        return {
+            'access_token': token,
+            'token_type': "bearer"
+        }
+    else:
+        return {"error": "Wrong credentials"}, 401
+
+api.add_resource(Route,
+    '/api/routes/<string:date>/<int:cai_id>/<int:assigned_to>')
 api.add_resource(RouteSuggestions, '/api/routes')
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/database/models.py
+++ backend/database/models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from peewee import *
+from flask_login import UserMixin
 
 psql_db = PostgresqlDatabase('route_patrol', user='postgres')
 
@@ -7,10 +8,13 @@ class BaseModel(Model):
     class Meta:
         database = psql_db
 
-class Manager(BaseModel):
+class Manager(BaseModel, UserMixin):
     cedula = CharField()
     password_hash = CharField()
     cai_id = IntegerField()
+
+    def get_id():
+        return cedula
 
 if __name__ == "__main__":
     psql_db.connect()
