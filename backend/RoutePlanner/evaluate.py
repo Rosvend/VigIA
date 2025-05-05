d85fce692034a39950ece7876ab88b148692bf1a
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-05-05 09:45:17 -0500

    Add evaluation result saving feature

--- backend/RoutePlanner/evaluate.py
+++ backend/RoutePlanner/evaluate.py
@@ -1,17 +1,23 @@
 from .default_router import PoliceRouter
 from .prediction.simple_wrapper import SimpleModelWrapper
+from argparse import ArgumentParser
 from config_test import ORS_KEY, MODEL_PATH
+from datetime import datetime
 from operator import itemgetter
+from os.path import exists
 from random import choices
 from shapely import LineString
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 
 MAX_STATIONS = 4
 MAX_ROUTES = 4
 ROUTE_LENGTH_WEIGHT = -0.1
 ROUTE_LENGTHVAR_WEIGHT = -0.02
 SCALER = 1000
+RESULTS_FILE = "evaluation_results.csv"
+TIMESTAMP_FORMAT = "%y-%m-%d %H:%M:%S"
 
 def mark_emptyroutes(points: list):
     if len(points) > 1:
@@ -42,8 +48,29 @@ def evaluate_router(router: PoliceRouter, **kwargs):
     return scores
 
 if __name__ == "__main__":
+    parser = ArgumentParser(
+        prog='evaluate.py',
+        description='Evaluate the performance of the routing algorithm'
+    )
+    parser.add_argument('-s', '--save', action='store_true', help=f'Store the results in {RESULTS_FILE}')
+    args = parser.parse_args()
+
     router = PoliceRouter(
         ORS_KEY, SimpleModelWrapper(MODEL_PATH)
     )
-    print(evaluate_router(router))
-    # print(evaluate_router(router, 3, 20])
+    results = evaluate_router(router)
+
+    if args.save:
+        if exists(RESULTS_FILE):
+            past_results = pd.read_csv(RESULTS_FILE)
+        else:
+            past_results = pd.DataFrame(columns=['timestamp',
+                *(f"score_{n+1}" for n in range(MAX_ROUTES))
+            ])
+        past_results.loc[len(past_results)] = \
+            [datetime.strftime(datetime.now(), TIMESTAMP_FORMAT)] \
+            + results
+        past_results.to_csv(RESULTS_FILE, index=False)
+       
+    else:
+        print(results)
